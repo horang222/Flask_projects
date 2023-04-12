ec8bb9e933625b2233d9c7750bb0107756fb2aaa
Park BJ <bjpark3321@gmail.com>
2023-04-12 16:52:46 +0900

    Flask study #2: page numbering, login page

--- myproject/pybo/views/question_views.py
+++ myproject/pybo/views/question_views.py
@@ -1,7 +1,8 @@
-from flask import Blueprint, render_template, request, url_for
+from flask import Blueprint, render_template, request, url_for, g
 from werkzeug.utils import redirect
 from pybo.forms import QuestionForm, AnswerForm
 from pybo.models import Question
+from pybo.views.auth_views import login_required
 from datetime import datetime
 from .. import db
 
@@ -10,7 +11,9 @@ bp = Blueprint('question', __name__, url_prefix='/question')
 
 @bp.route('/list/')
 def _list():
+    page = request.args.get('page', type=int, default=1)  # 페이지
     question_list = Question.query.order_by(Question.create_date.desc())
+    question_list = question_list.paginate(page=page, per_page=10)
     return render_template('question/question_list.html', question_list=question_list)
 
 
@@ -21,10 +24,11 @@ def detail(question_id):
     return render_template('question/question_detail.html', question=question, form=form)
 
 @bp.route('/create/', methods=('GET', 'POST'))
+@login_required
 def create():
     form = QuestionForm()
     if request.method == 'POST' and form.validate_on_submit():
-        question = Question(subject=form.subject.data, content=form.content.data, create_date=datetime.now())
+        question = Question(subject=form.subject.data, content=form.content.data, create_date=datetime.now(), user=g.user)
         db.session.add(question)
         db.session.commit()
         return redirect(url_for('main.index'))
